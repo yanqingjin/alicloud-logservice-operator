b5d03c5cc7c208a2dbf0269b030c0cfd095d5852
Leon Jin <yanqing.jin@philips.com>
2020-10-29 13:00:47 +0800

    upgrade operator-sdk to 1.1.0

--- main.go
+++ main.go
@@ -75,6 +75,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LogProject")
 		os.Exit(1)
 	}
+	if err = (&controllers.LogStoreReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("LogStore"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LogStore")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
